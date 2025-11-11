bf2904e23f7091bc63e69a7d7cee96c6a79e75e5
ValentinKranz <valentin.kranz@gmail.com>
2025-11-11 14:25:54 -0300

    modify README.md and main.py to count words

---
+++ main.py
@@ -0,0 +1,23 @@
+#main function
+def main():
+    book_path = "/home/valentinkranz/workspace/github.com/bootdev/bookbot/books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"Found {num_words} total words")
+
+
+#Function to read the content of a book from a text file and return it as a string
+def get_book_text(file_path):
+    # Open the file
+    with open(file_path) as file:
+        # Read the content of the file
+        return file.read()
+        
+
+#Function to count words in file
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+main()  
